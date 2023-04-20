7ff5d380904487f19e3a42498f126adda855c786
lasarinii <pedroluislasarini23@gmail.com>
2023-04-20 17:17:01 -0300

    adição da rota de produtos

--- main.go
+++ main.go
@@ -9,7 +9,21 @@ import (
 )
 
 var PORT string = ":8080"
-var url string = "https://uselessfacts.jsph.pl/api/v2/facts/random?lang=en"
+var factsURL string = "https://uselessfacts.jsph.pl/api/v2/facts/random?lang=en"
+var productsURL string = "https://dummyjson.com/products"
+
+type Product struct {
+	Id          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       int     `json:"price"`
+	Rating      float32 `json:"rating"`
+	Brand       string  `json:"brand"`
+	Category    string  `json:"category"`
+}
+type ProductList struct {
+	Products []Product `json:"products"`
+}
 
 type Fact struct {
 	Id         string `json:"id"`
@@ -20,8 +34,38 @@ type Fact struct {
 	Permalink  string `json:"permalink"`
 }
 
+func getProducts() (productsStr string) {
+	resp, reqErr := http.Get(productsURL)
+	if reqErr != nil {
+		log.Fatalln(reqErr)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, convertErr := ioutil.ReadAll(resp.Body)
+	if convertErr != nil {
+		log.Fatalln(convertErr)
+	}
+
+	var products ProductList
+
+	unmarshalErr := json.Unmarshal(bodyBytes, &products)
+	if unmarshalErr != nil {
+		log.Fatalln(unmarshalErr)
+	}
+
+	productsBytes, marshalErr := json.Marshal(products.Products)
+	if marshalErr != nil {
+		log.Fatalln(marshalErr)
+	}
+
+	productsStr = string(productsBytes)
+
+	return
+}
+
 func getFacts() (factStr string) {
-	resp, reqErr := http.Get(url)
+	resp, reqErr := http.Get(factsURL)
 	if reqErr != nil {
 		log.Fatalln(reqErr)
 	}
@@ -50,19 +94,19 @@ func getFacts() (factStr string) {
 	return
 }
 
-func handleReq(w http.ResponseWriter, req *http.Request) {
+func factsReq(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, getFacts())
 }
 
+func productsReq(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, getProducts())
+}
+
 func main() {
-	server := &http.Server{
-		Addr:    PORT,
-		Handler: http.HandlerFunc(handleReq),
-	}
+	http.HandleFunc("/facts", factsReq)
+	http.HandleFunc("/products", productsReq)
 
 	fmt.Printf("Running on port %v", PORT)
 
-	if serverErr := server.ListenAndServe(); serverErr != nil {
-		log.Fatalln(serverErr)
-	}
+	http.ListenAndServe(PORT, nil)
 }
